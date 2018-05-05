622c028b2ba45b4511518166d309f01d45e3a6a1
Dominic Critchlow <dom@Dominics-MacBook-Pro.local>
2018-05-05 19:32:01 -0400

    update senate

--- data_manage.py
+++ data_manage.py
@@ -33,19 +33,63 @@ def member_dataframe(congress='115',chamber="senate"):
     df = pd.DataFrame.from_dict(dec)
     return df
 
+def clean_house(mdf):
+    mask = mdf.district == "At-Large"
+    mdf.loc[mask, 'district'] = '00'
+    mdf['district'] = mdf['district'].apply(lambda x: x.zfill(2))
+    
+    mdf['ident']= mdf['state']+ mdf['district'] 
+    mdf['ident'] = mdf['ident'].astype(str)
+    mdf.set_index('ident',inplace=True)
+    
+    mdf = clean(mdf)
+    
+    return mdf
+
+def clean(mdf):
+    mdf['youtube_account']= mdf['youtube_account'].replace(np.nan, '', regex=True)
+    mdf['twitter_account']= mdf['twitter_account'].replace(np.nan, '', regex=True)
+    mdf['facebook_account']= mdf['facebook_account'].replace(np.nan, '', regex=True)
+    
+    mask = mdf['party'] == "R"
+    mdf.loc[mask, 'party'] = 'Republican'
+    
+    mask = mdf['party'] == "D"
+    mdf.loc[mask, 'party'] = 'Democrat'
+    
+    mask = mdf['party'] == "I"
+    mdf.loc[mask, 'party'] = 'Independent'
+    return mdf
+
+def clean_shape(gdf):
+    gdf['ident'] = gdf['STATEFP_let'] + gdf['CD115FP']
+    gdf['ident'] = gdf['ident'].astype(str)
+    gdf.set_index('ident',inplace=True)
+    return gdf
+    
+def join_mem_shape(gdf,mdf):
+    total_df = gdf.join(mdf,how='left',lsuffix='_l',rsuffix='_r')
+    return total_df
+
+def join_senate_shape(gdf,mdf):
+    gdf.set_index('STATE_ABBR',inplace= True)
+    mdf.set_index('state',inplace=True)
+    total_df = gdf.join(mdf,how='left',lsuffix='_l',rsuffix='_r')
+    return total_df
 
 def load_shape(congress):
     if congress == "house":
         geodf = geopandas.read_file("Shapefiles/"+congress+"/cb_2016_us_cd115_500k.shp")
+        geodf['STATEFP_let'] = geodf['STATEFP']
+        geodf = geodf.replace({"STATEFP_let": state_fs_num})   
     else:
         geodf = geopandas.read_file("Shapefiles/"+congress+"/states.shp")
         
-    geodf['STATEFP_let'] = geodf['STATEFP']
-    geodf = geodf.replace({"STATEFP_let": state_fs_num})    
+     
     
     return geodf
 
-def simple_map_html(gdf, outname):
+def simple_map_html(gdf):
     
     def style_function(feature):
         return {
@@ -62,9 +106,10 @@ def simple_map_html(gdf, outname):
     
     c = folium.GeoJson(gdf,style_function=style_function)
     b.add_child(c)
-    b.save(outname)
     
-def party_map_html(gdf, outname):
+    return b
+    
+def party_map_html(gdf):
     
     def style_function_rep(feature):
         return {
@@ -80,23 +125,33 @@ def party_map_html(gdf, outname):
             'weight': .5
         }
     
+    def style_function_ind(feature):
+        return {
+            'fillColor': 'green',
+            'color': 'black',
+            'weight': .5
+        }
+    
     b = folium.Map([40.7, -74],
                    zoom_start=3,
                    tiles='cartodbpositron',
                    world_copy_jump=True,
                    detect_retina = True)
     
-    rep = gdf[gdf['party'] == 'R']
-    dem = gdf[gdf['party'] == 'D']
+    rep = gdf[gdf['party'] == 'Republican']
+    dem = gdf[gdf['party'] == 'Democrat']
+    ind = gdf[gdf['party'] == 'Independent']
     
     rep_map = folium.GeoJson(rep,style_function=style_function_rep)
     dem_map = folium.GeoJson(dem,style_function=style_function_dem)
+    ind_map = folium.GeoJson(ind,style_function=style_function_ind)
     
     b.add_child(rep_map)
     b.add_child(dem_map)
-    b.save(outname)
+    b.add_child(ind_map)
+    return b
 
-def party_map_html_pop(gdf, outname):
+def party_map_html_pop(gdf):
     
     html_img = '''
     
@@ -163,6 +218,13 @@ button:hover, a:hover {{
             'weight': .5
         }
     
+    def style_function_ind(feature):
+        return {
+            'fillColor': 'green',
+            'color': 'black',
+            'weight': .5
+        }
+    
     def style_function_dem(feature):
         return {
             'fillColor': 'red',
@@ -176,16 +238,16 @@ button:hover, a:hover {{
                    world_copy_jump=True,
                    detect_retina = True)
     
-    rep = gdf[gdf['party'] == 'R']
-    dem = gdf[gdf['party'] == 'D']
+    rep = gdf[gdf['party'] == 'Republican']
+    dem = gdf[gdf['party'] == 'Democrat']
+    ind = gdf[gdf['party'] == 'Independent']
     
     fg_rep = folium.FeatureGroup(name="Republicans")
     fg_dem = folium.FeatureGroup(name="Democrates")
-    
+    fg_ind = folium.FeatureGroup(name="Independent")
     
     u_id = rep['id'].unique()
     for i_id in u_id:
-        #print(i_id)
         rep_map = folium.GeoJson(rep[rep['id']==i_id],style_function=style_function_rep)
         folium.Popup(html_img("mem_img/"+rep[rep['id']==i_id]['id'].min()+".jpg",
                     rep[rep['id']==i_id]['first_name'].min(),
@@ -201,7 +263,6 @@ button:hover, a:hover {{
         
     u_id = dem['id'].unique()
     for i_id in u_id:
-        #print(i_id)
         dem_map = folium.GeoJson(dem[dem['id']==i_id],style_function=style_function_dem)
         folium.Popup(html_img("mem_img/"+dem[dem['id']==i_id]['id'].min()+".jpg",
                     dem[dem['id']==i_id]['first_name'].min(),
@@ -214,9 +275,29 @@ button:hover, a:hover {{
                     ).add_to(dem_map)
         dem_map.add_to(fg_dem)
     
+    u_id = ind['id'].unique()
+    for i_id in u_id:
+        ind_map = folium.GeoJson(ind[ind['id']==i_id],style_function=style_function_ind)
+        folium.Popup(html_img("mem_img/"+ind[ind['id']==i_id]['id'].min()+".jpg",
+                    ind[ind['id']==i_id]['first_name'].min(),
+                    str(ind[ind['id']==i_id]['last_name'].min().replace("'"," ")),
+                    ind[ind['id']==i_id]['title'].min(),
+                    ind[ind['id']==i_id]['party'].min(),
+                             ind[ind['id']==i_id]['twitter_account'].min(),
+                             ind[ind['id']==i_id]['facebook_account'].min(),
+                             ind[ind['id']==i_id]['youtube_account'].min())
+                    ).add_to(ind_map)
+        ind_map.add_to(fg_ind)
+    
+    
     b.add_child(fg_rep)
     b.add_child(fg_dem)
-    b.save(outname)    
+    b.add_child(fg_ind)
+    folium.LayerControl().add_to(b)
+    return b
+    
+def write_map(b_map,outname):
+    b_map.save(outname)
 
 def member_img(bioguide):
     img_url ="https://theunitedstates.io/images/congress/original/"+ bioguide +".jpg"
